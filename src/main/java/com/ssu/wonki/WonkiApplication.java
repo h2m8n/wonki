5eb210d6d6329d600b30b6d703da3be2b989373a
HeeMun Song <h2m8n.song@gmail.com>
2022-10-18 13:46:45 +0900

    project init

---
+++ src/main/java/com/ssu/wonki/WonkiApplication.java
@@ -0,0 +1,13 @@
+package com.ssu.wonki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WonkiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WonkiApplication.class, args);
+	}
+
+}
